e1454d7d0d848dc02059fadfd83e91114a0b55f5
NipunEranda <namarasekara71@gmail.com>
2019-04-16 10:46:44 +0000

    Login Success

--- src/com/sliit/LoginController.java
+++ src/com/sliit/LoginController.java
@@ -13,40 +13,59 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-
 import javax.servlet.annotation.WebServlet;
-
 
 @SuppressWarnings("serial")
 @WebServlet("/LoginController")
 public class LoginController extends HttpServlet {
-	
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String un=request.getParameter("email");
-		String pw=request.getParameter("password");
-		
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String un = request.getParameter("email");
+		String pw = request.getParameter("password");
+
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
-		Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/MusicStore", "root", "4212");
- 
-		PreparedStatement ps = c.prepareStatement("select userName,password from Users where email=? and password =?");
-		ps.setString(1, un);
-		ps.setString(2, pw);
- 
-		ResultSet rs = ps.executeQuery();
- 
-		while (rs.next()) {
-			response.sendRedirect("homePage.jsp");
-			HttpSession session = request.getSession();
-			session.setAttribute("name", un);
+			Connection c = DriverManager.getConnection("jdbc:mysql://localhost:3306/MusicStore", "root", "4212");
+
+			PreparedStatement ps = c.prepareStatement("select userName,isAdmin from Users where email=? or userName=? and password =?");
+			ps.setString(1, un);
+			ps.setString(2, un);
+			ps.setString(3, pw);
+
+			ResultSet rs = ps.executeQuery();
+
+			while (rs.next()) {
+				String userName = rs.getString("userName");
+				int isAdmin = rs.getInt("isAdmin");
+
+				if (userName == "") {
+					userName = un;
+					HttpSession session = request.getSession();
+					session.setAttribute("name", un);
+				} else {
+					HttpSession session = request.getSession();
+					session.setAttribute("name", userName);
+				}
+
+				if (isAdmin == 0) {
+					response.sendRedirect("homePage.jsp");
+				} else {
+					response.sendRedirect("admin/adminHome.jsp");
+				}
+
+				return;
+			}
+
+			rs.close();
+			ps.close();
+			c.close();
+
+			response.sendRedirect("login.jsp");
 			return;
-		}
-		response.sendRedirect("login.jsp");
-		return;
 		} catch (ClassNotFoundException | SQLException e) {
 			e.printStackTrace();
 		}
 	}
 
 }
-
