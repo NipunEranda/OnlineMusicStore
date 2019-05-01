8113e5fc2dabc604d0761faf81424cd53f1224e3
Nipun <namarasekara71@gmail.com>
2019-05-01 09:30:25 +0530

    DBupdated

--- WebContent/DB/MusicStore.sql
+++ WebContent/DB/MusicStore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: Apr 15, 2019 at 07:48 PM
+-- Generation Time: May 01, 2019 at 09:24 AM
 -- Server version: 5.7.25-0ubuntu0.18.04.2
 -- PHP Version: 7.2.15-0ubuntu0.18.04.2
 
@@ -29,25 +29,25 @@ SET time_zone = "+00:00";
 CREATE TABLE `Users` (
   `uId` int(11) NOT NULL,
   `userName` varchar(25) NOT NULL,
-  `firstName` varchar(255) DEFAULT NULL,
-  `lastName` varchar(255) DEFAULT NULL,
-  `email` varchar(255) DEFAULT NULL,
+  `firstName` varchar(255) NOT NULL,
+  `lastName` varchar(255) NOT NULL,
+  `email` varchar(255) NOT NULL,
   `password` varchar(25) NOT NULL,
-  `image` varchar(255) NOT NULL,
+  `image` varchar(1000) NOT NULL,
   `telephone` varchar(10) NOT NULL,
   `postalCode` int(6) NOT NULL,
   `address` varchar(255) NOT NULL,
-  `in_staff` tinyint(1) NOT NULL,
-  `is_owner` tinyint(1) NOT NULL,
-  `isAdmin` int(11) DEFAULT NULL
+  `isAdmin` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `Users`
 --
 
-INSERT INTO `Users` (`uId`, `userName`, `firstName`, `lastName`, `email`, `password`, `image`, `telephone`, `postalCode`, `address`, `in_staff`, `is_owner`, `isAdmin`) VALUES
-(1, 'Nipun', 'Nipun', 'Amarasekara', 'root', '4212', '', '', 0, '', 0, 0, 1);
+INSERT INTO `Users` (`uId`, `userName`, `firstName`, `lastName`, `email`, `password`, `image`, `telephone`, `postalCode`, `address`, `isAdmin`) VALUES
+(1, 'Nipun', 'Nipun', 'Amarasekara', 'root', '4212', 'eafeafegegar', '0770415266', 5555, 'r4qt4t4bt', 1),
+(2, 'Neo', 'Neo', 'Amarasekara', 'namarasekara71@gmail.com', '4212', 'fefwgwrg', '0770415266', 8080, 'ewwefefwegweg', 0),
+(4, 'Nipun_x71', 'Nipun', 'Amarasekara', 'root', '4212', 'images/userProfiles/Nipun.jpg', '0770415266', 1000, 'r4qt4t4bt', 0);
 
 --
 -- Indexes for dumped tables
@@ -59,6 +59,15 @@ INSERT INTO `Users` (`uId`, `userName`, `firstName`, `lastName`, `email`, `passw
 ALTER TABLE `Users`
   ADD PRIMARY KEY (`uId`);
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `Users`
+--
+ALTER TABLE `Users`
+  MODIFY `uId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
