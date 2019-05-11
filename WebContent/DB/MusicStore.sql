0ad2a92411a22eb521b0f2ec1ddca3a5af7f8d70
NipunEranda <38685839+NipunEranda@users.noreply.github.com>
2019-05-11 11:48:49 +0530

    DBUpdate

--- WebContent/DB/MusicStore.sql
+++ WebContent/DB/MusicStore.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Generation Time: May 01, 2019 at 09:24 AM
+-- Generation Time: May 11, 2019 at 11:48 AM
 -- Server version: 5.7.25-0ubuntu0.18.04.2
 -- PHP Version: 7.2.15-0ubuntu0.18.04.2
 
@@ -19,6 +19,84 @@ SET time_zone = "+00:00";
 --
 -- Database: `MusicStore`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Albums`
+--
+
+CREATE TABLE `Albums` (
+  `AlbumId` int(10) NOT NULL,
+  `AlbumName` varchar(255) NOT NULL,
+  `AlbumDescription` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Albums`
+--
+
+INSERT INTO `Albums` (`AlbumId`, `AlbumName`, `AlbumDescription`) VALUES
+(1, 'lol', '3r23rewf');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Artists`
+--
+
+CREATE TABLE `Artists` (
+  `ArtistId` int(10) NOT NULL,
+  `ArtistName` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Genres`
+--
+
+CREATE TABLE `Genres` (
+  `GenreId` int(255) NOT NULL,
+  `GenreName` varchar(255) NOT NULL,
+  `GenreDescription` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `Genres`
+--
+
+INSERT INTO `Genres` (`GenreId`, `GenreName`, `GenreDescription`) VALUES
+(1, 'Rock', 'hrtdhtyjr'),
+(2, 'jazz', ''),
+(3, 'Electronic', ''),
+(4, 'Dubstep', 'gwgwergrw'),
+(5, 'Rhythm', ''),
+(6, 'Techno', ''),
+(7, 'HipHop', ''),
+(8, 'Pop', ''),
+(9, 'Country', ''),
+(10, 'Classical', ''),
+(11, 'Heavy Metal', ''),
+(12, 'Dance', ''),
+(13, 'Rap', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Tracks`
+--
+
+CREATE TABLE `Tracks` (
+  `TrackId` int(11) NOT NULL,
+  `TrackName` varchar(255) DEFAULT NULL,
+  `AlbumId` int(11) DEFAULT NULL,
+  `GenreId` int(11) DEFAULT NULL,
+  `Published` date NOT NULL,
+  `Composer` varchar(255) DEFAULT NULL,
+  `Milliseconds` int(11) DEFAULT NULL,
+  `UnitPrice` double DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -46,12 +124,38 @@ CREATE TABLE `Users` (
 
 INSERT INTO `Users` (`uId`, `userName`, `firstName`, `lastName`, `email`, `password`, `image`, `telephone`, `postalCode`, `address`, `isAdmin`) VALUES
 (1, 'Nipun', 'Nipun', 'Amarasekara', 'root', '4212', 'eafeafegegar', '0770415266', 5555, 'r4qt4t4bt', 1),
-(2, 'Neo', 'Neo', 'Amarasekara', 'namarasekara71@gmail.com', '4212', 'fefwgwrg', '0770415266', 8080, 'ewwefefwegweg', 0),
-(4, 'Nipun_x71', 'Nipun', 'Amarasekara', 'root', '4212', 'images/userProfiles/Nipun.jpg', '0770415266', 1000, 'r4qt4t4bt', 0);
+(2, 'Nadee', 'Nadeera', 'Amarasekara', 'nadeera94@gmail.com', '4212', 'fefwgwrg', '0770415266', 8080, 'ewwefefwegweg', 0),
+(4, 'Neo_71', 'Nipun', 'Amarasekara', 'root', '4212', 'images/userProfiles/Nipun.jpg', '0770415266', 1000, 'r4qt4t4bt', 0);
 
 --
 -- Indexes for dumped tables
 --
+
+--
+-- Indexes for table `Albums`
+--
+ALTER TABLE `Albums`
+  ADD PRIMARY KEY (`AlbumId`);
+
+--
+-- Indexes for table `Artists`
+--
+ALTER TABLE `Artists`
+  ADD PRIMARY KEY (`ArtistId`);
+
+--
+-- Indexes for table `Genres`
+--
+ALTER TABLE `Genres`
+  ADD PRIMARY KEY (`GenreId`);
+
+--
+-- Indexes for table `Tracks`
+--
+ALTER TABLE `Tracks`
+  ADD PRIMARY KEY (`TrackId`),
+  ADD KEY `AlbumId_fk` (`AlbumId`),
+  ADD KEY `GenreId_fk` (`GenreId`);
 
 --
 -- Indexes for table `Users`
@@ -64,10 +168,36 @@ ALTER TABLE `Users`
 --
 
 --
+-- AUTO_INCREMENT for table `Albums`
+--
+ALTER TABLE `Albums`
+  MODIFY `AlbumId` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT for table `Artists`
+--
+ALTER TABLE `Artists`
+  MODIFY `ArtistId` int(10) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT for table `Genres`
+--
+ALTER TABLE `Genres`
+  MODIFY `GenreId` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
+--
 -- AUTO_INCREMENT for table `Users`
 --
 ALTER TABLE `Users`
   MODIFY `uId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `Tracks`
+--
+ALTER TABLE `Tracks`
+  ADD CONSTRAINT `AlbumId_fk` FOREIGN KEY (`AlbumId`) REFERENCES `Albums` (`AlbumId`),
+  ADD CONSTRAINT `GenreId_fk` FOREIGN KEY (`GenreId`) REFERENCES `Genres` (`GenreId`);
+
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
